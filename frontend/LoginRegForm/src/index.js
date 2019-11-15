749f6d3b83483d83ccf9a4c88d0d06bdac339e6e
m1xy <57626542+m1xy@users.noreply.github.com>
2019-11-15 02:16:09 +0100

    Add files via upload
    Login, registracija, HomePage od lekara- frontend

---
+++ frontend/LoginRegForm/src/index.js
@@ -0,0 +1,14 @@
+var app = new Vue({ 
+   el: "#root",
+  data : {
+  	korisnik: "korisnik",
+    sifra: "sdfs",
+    ime: "",
+    prezime: "",
+    ardresa: "",
+    grad: "",
+    drzava: "",
+    brTel: "",
+    brOsiguranika: ""
+  }
+});
